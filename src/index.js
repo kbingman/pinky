21dc60c005266c30057ac501f7a5b8bb7caa9867
Keith Bingman <kb@wantering.com>
2016-06-04 22:19:23 -0700

    setup

--- src/index.js
+++ src/index.js
@@ -1,21 +1,2 @@
-/**
- * Basic fullscreen canvas
- */
-export function initCanvas (canvas, { width, height, ratio }) {
-  const context = canvas.getContext('2d');
-
-  canvas.width = width * ratio;
-  canvas.height = height * ratio;
-  canvas.style.width = width + 'px';
-  canvas.style.height = height + 'px';
-  context.scale(ratio, ratio);
-  
-  return context;
-}
-
-/**
- * Clear canvas
- */
-export function clearCanvas (context, { width, height }) {
-  context.clearRect(0, 0, width, height);
-};
+export { initCanvas as initCanvas  } from './canvas';
+export { clearCanvas as clearCanvas  } from './canvas';
